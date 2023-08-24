588f08d5c1c36cb7655c7557ded2553db4f8a56d
Justin <jspivack2198@gmail.com>
2023-08-24 17:48:50 -0400

    push

---
+++ app.js
@@ -0,0 +1,50 @@
+var a = 5
+var b = 10
+
+function quotient(x,y){
+    return y / x
+}
+
+console.log(quotient(a,b))
+
+function product(x,y){
+    return x * y
+}
+
+console.log(product(a,b))
+
+function diff(x,y){
+    return x - y
+}
+
+console.log(diff(a,b))
+
+function add(x,y){
+    return x + y;
+}
+
+console.log(add(a,b))
+
+const quotient = (x,y) =>{
+    return y/x;
+}
+
+console.log(quotient(a,b))
+
+const product = (x,y) =>{
+    return x*y;
+}
+
+console.log(product(a,b))
+
+const diff = (x,y) =>{
+    return x-y;
+}
+
+console.log(diff(a,b))
+
+const add = (x,y) =>{
+    return x+y;
+}
+
+console.log(add(a,b))
